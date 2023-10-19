cd8bfccd7329146dae993d422037d28042216651
maksim kireev <maksimkir987@gmail.com>
2023-10-19 22:27:01 +0300

    add task 2

---
+++ task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string namber = Console.ReadLine();
+
+
+int sumNamber(string namber){
+    int result = 0;
+    for (int i = 0; i < namber.Length; i++) result += int.Parse(namber[i].ToString());
+    return result;
+}
+
+int result = sumNamber(namber);
+Console.WriteLine(result);
