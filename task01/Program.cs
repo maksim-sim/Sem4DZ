e21640269e3017db023b31e461c9f700f848ebd6
maksim kireev <maksimkir987@gmail.com>
2023-10-19 22:26:41 +0300

    add task 1

---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите cтепень в которую необходимо возвести число {namber}: ");
+int degreeNam = Convert.ToInt32(Console.ReadLine());
+
+
+double erectDegree(int namber, int degreeNam){
+    double result = 1;
+    for(int i = 0; i<degreeNam; i++) result*=namber;
+    return result;
+}
+
+Console.WriteLine(erectDegree(namber, degreeNam));
